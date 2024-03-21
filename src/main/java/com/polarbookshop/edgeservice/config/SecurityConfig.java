ba29ef37af61a4e510503474b316f2450a0924da
Johannes Priebsch <johannes@priebsch.at>
2024-03-21 11:33:22 +0100

    add spring-security with form-login and auto-generated password

---
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.polarbookshop.edgeservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+
+        return httpSecurity
+                .authorizeExchange(exchange ->
+                        exchange.anyExchange().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
