47166c7fc1d64bb5d8fb58d0637e7f3f159ff5a8
Johannes Priebsch <johannes@priebsch.at>
2024-03-21 20:48:23 +0100

    add oauth2Login and logoutSuccessHandler, and User-based RateLimiterConfig

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -5,18 +5,41 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+    SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity httpSecurity,
+            ReactiveClientRegistrationRepository clientRegistrationRepository
+    ) {
 
         return httpSecurity
                 .authorizeExchange(exchange ->
                         exchange.anyExchange().authenticated())
-                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logoutSpec -> logoutSpec.logoutSuccessHandler(
+                        oidcLogoutSuccessHandler(clientRegistrationRepository)
+                ))
                 .build();
+    }
+
+    private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(
+            ReactiveClientRegistrationRepository clientRegistrationRepository) {
+
+        var oidcLogoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUri}");
+        return oidcLogoutSuccessHandler;
+
     }
 }
